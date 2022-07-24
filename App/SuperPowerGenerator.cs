7763309446660193e674fd3d05fbbe9af04c1d75
GHTeodor <Theodor.Lviv@gmail.com>
2022-07-24 15:55:14 +0300

    create feature1 branch

---
+++ App/SuperPowerGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGame.App
+{
+    public class SuperPowerGenerator
+    {
+        public static int GetRandom()
+        {
+            return new Random().Next(5, 11);
+        }
+    }
+}
