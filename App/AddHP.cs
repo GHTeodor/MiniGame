26f1bfc87ec2580b7fa20b5a2455d8953657184d
GHTeodor <Theodor.Lviv@gmail.com>
2022-07-24 20:44:24 +0300

    Create Feature3 branch

---
+++ App/AddHP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniGame.App
+{
+    public class AddHP : ISuperPower
+    {
+        public virtual int AddSuperPower(Warrior warrior, int hp)
+        {
+            return warrior.HP += hp;
+        }
+    }
+}
